2937062531922d5ea20d8e057c7d1961fad0b28c
Elvis Melkić <elvis.melkic@gmail.com>
2021-05-09 21:05:37 +0200

    Fix names

--- test/vanado_backend/failures_test.exs
+++ test/vanado_backend/failures_test.exs
@@ -7,62 +7,66 @@ defmodule VanadoBackend.FailuresTest do
     alias VanadoBackend.Failures.Failure
 
     @valid_attrs %{description: "some description", is_fixed: true, name: "some name"}
-    @update_attrs %{description: "some updated description", is_fixed: false, name: "some updated name"}
+    @update_attrs %{
+      description: "some updated description",
+      is_fixed: false,
+      name: "some updated name"
+    }
     @invalid_attrs %{description: nil, is_fixed: nil, name: nil}
 
     def failure_fixture(attrs \\ %{}) do
       {:ok, failure} =
         attrs
         |> Enum.into(@valid_attrs)
-        |> Failures.create_failure()
+        |> Failures.create()
 
       failure
     end
 
-    test "list_failures/0 returns all failures" do
+    test "list/0 returns all failures" do
       failure = failure_fixture()
-      assert Failures.list_failures() == [failure]
+      assert Failures.list() == [failure]
     end
 
-    test "get_failure!/1 returns the failure with given id" do
+    test "get!/1 returns the failure with given id" do
       failure = failure_fixture()
-      assert Failures.get_failure!(failure.id) == failure
+      assert Failures.get!(failure.id) == failure
     end
 
-    test "create_failure/1 with valid data creates a failure" do
-      assert {:ok, %Failure{} = failure} = Failures.create_failure(@valid_attrs)
+    test "create/1 with valid data creates a failure" do
+      assert {:ok, %Failure{} = failure} = Failures.create(@valid_attrs)
       assert failure.description == "some description"
       assert failure.is_fixed == true
       assert failure.name == "some name"
     end
 
-    test "create_failure/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Failures.create_failure(@invalid_attrs)
+    test "create/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Failures.create(@invalid_attrs)
     end
 
-    test "update_failure/2 with valid data updates the failure" do
+    test "update/2 with valid data updates the failure" do
       failure = failure_fixture()
-      assert {:ok, %Failure{} = failure} = Failures.update_failure(failure, @update_attrs)
+      assert {:ok, %Failure{} = failure} = Failures.update(failure, @update_attrs)
       assert failure.description == "some updated description"
       assert failure.is_fixed == false
       assert failure.name == "some updated name"
     end
 
-    test "update_failure/2 with invalid data returns error changeset" do
+    test "update/2 with invalid data returns error changeset" do
       failure = failure_fixture()
-      assert {:error, %Ecto.Changeset{}} = Failures.update_failure(failure, @invalid_attrs)
-      assert failure == Failures.get_failure!(failure.id)
+      assert {:error, %Ecto.Changeset{}} = Failures.update(failure, @invalid_attrs)
+      assert failure == Failures.get!(failure.id)
     end
 
-    test "delete_failure/1 deletes the failure" do
+    test "delete/1 deletes the failure" do
       failure = failure_fixture()
-      assert {:ok, %Failure{}} = Failures.delete_failure(failure)
-      assert_raise Ecto.NoResultsError, fn -> Failures.get_failure!(failure.id) end
+      assert {:ok, %Failure{}} = Failures.delete(failure)
+      assert_raise Ecto.NoResultsError, fn -> Failures.get!(failure.id) end
     end
 
-    test "change_failure/1 returns a failure changeset" do
+    test "change/1 returns a failure changeset" do
       failure = failure_fixture()
-      assert %Ecto.Changeset{} = Failures.change_failure(failure)
+      assert %Ecto.Changeset{} = Failures.change(failure)
     end
   end
 end
