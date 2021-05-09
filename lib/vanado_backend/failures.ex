d13753149ad88e872c7e2e12d9ff16fd3cca26fd
Elvis Melkić <elvis.melkic@gmail.com>
2021-05-09 20:56:00 +0200

    Remove comments noise

--- lib/vanado_backend/failures.ex
+++ lib/vanado_backend/failures.ex
@@ -10,12 +10,6 @@ defmodule VanadoBackend.Failures do
 
   @doc """
   Returns the list of failures conditionally filtered by `is_fixed` field.
-
-  ## Examples
-
-      iex> list_failures()
-      [%Failure{}, ...]
-
   """
   def list_failures(is_fixed) do
     Failure
@@ -26,31 +20,11 @@ defmodule VanadoBackend.Failures do
 
   @doc """
   Gets a single failure.
-
-  Raises `Ecto.NoResultsError` if the Failure does not exist.
-
-  ## Examples
-
-      iex> get_failure!(123)
-      %Failure{}
-
-      iex> get_failure!(456)
-      ** (Ecto.NoResultsError)
-
   """
   def get_failure!(id), do: Repo.get!(Failure, id)
 
   @doc """
   Creates a failure.
-
-  ## Examples
-
-      iex> create_failure(%{field: value})
-      {:ok, %Failure{}}
-
-      iex> create_failure(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def create_failure(attrs \\ %{}) do
     %Failure{}
@@ -60,15 +34,6 @@ defmodule VanadoBackend.Failures do
 
   @doc """
   Updates a failure.
-
-  ## Examples
-
-      iex> update_failure(failure, %{field: new_value})
-      {:ok, %Failure{}}
-
-      iex> update_failure(failure, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def update_failure(%Failure{} = failure, attrs) do
     failure
@@ -78,15 +43,6 @@ defmodule VanadoBackend.Failures do
 
   @doc """
   Deletes a failure.
-
-  ## Examples
-
-      iex> delete_failure(failure)
-      {:ok, %Failure{}}
-
-      iex> delete_failure(failure)
-      {:error, %Ecto.Changeset{}}
-
   """
   def delete_failure(%Failure{} = failure) do
     Repo.delete(failure)
@@ -94,12 +50,6 @@ defmodule VanadoBackend.Failures do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking failure changes.
-
-  ## Examples
-
-      iex> change_failure(failure)
-      %Ecto.Changeset{data: %Failure{}}
-
   """
   def change_failure(%Failure{} = failure, attrs \\ %{}) do
     Failure.changeset(failure, attrs)
