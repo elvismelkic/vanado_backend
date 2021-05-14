c6630c89cf236deb5a5916137da8eeeef8962b88
Elvis Melkić <elvis.melkic@gmail.com>
2021-05-14 22:57:04 +0200

    Fix loading static files

--- lib/vanado_backend_web/endpoint.ex
+++ lib/vanado_backend_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule VanadoBackendWeb.Endpoint do
     at: "/files",
     from: :vanado_backend,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only_matching: ~w(failure)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
