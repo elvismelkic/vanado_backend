126004c0f79cc015e3a0908813c9830b80f6d987
Elvis Melkić <elvis.melkic@gmail.com>
2021-05-14 23:59:13 +0200

    Send files in response

--- lib/vanado_backend_web/controllers/file_controller.ex
+++ lib/vanado_backend_web/controllers/file_controller.ex
@@ -2,24 +2,24 @@ defmodule VanadoBackendWeb.FileController do
   use VanadoBackendWeb, :controller
 
   alias VanadoBackend.Files
-  alias VanadoBackend.Files.File
 
   action_fallback VanadoBackendWeb.FallbackController
 
   def create(conn, params) do
-    with {:ok, %File{} = file} <- Files.create(params) do
+    with {:ok, files} <- Files.create(params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.file_path(conn, :show, file))
-      |> render("show.json", file: file)
+      |> render("index.json", files: files)
     end
   end
 
   def delete(conn, %{"id" => id}) do
     file = Files.get!(id)
 
-    with {:ok, %File{}} <- Files.delete(file) do
-      send_resp(conn, :no_content, "")
+    with {:ok, files} <- Files.delete(file) do
+      conn
+      |> put_status(:ok)
+      |> render("index.json", files: files)
     end
   end
 end
