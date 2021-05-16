5eeb60d10bbaf3bae5178e5a04aca064ba3bf725
Elvis Melkić <elvis.melkic@gmail.com>
2021-05-16 21:49:07 +0200

    Add bang to function names

--- lib/vanado_backend/api/file.ex
+++ lib/vanado_backend/api/file.ex
@@ -8,21 +8,21 @@ defmodule VanadoBackend.Api.File do
   @doc """
   Creates folder along with its parents.
   """
-  def create_folder_with_parents(path) do
+  def create_folder_with_parents!(path) do
     File.mkdir_p!("#{@root_path}#{path}")
   end
 
   @doc """
   Creates a file.
   """
-  def create_file(source_file_path, destination_file_path) do
+  def create_file!(source_file_path, destination_file_path) do
     File.cp!(source_file_path, "#{@root_path}#{destination_file_path}")
   end
 
   @doc """
   Deletes a file.
   """
-  def delete(path) do
+  def delete!(path) do
     File.rm!("#{@root_path}#{path}")
   end
 end
